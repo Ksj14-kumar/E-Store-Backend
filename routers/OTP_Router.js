cc2aa5d05d006f10a394c2e0d14771e1e070a635
ksj14-kumar <lionmassymassy@gmail.com>
2023-03-06 18:27:18 +0530

    remove bugs

--- routers/OTP_Router.js
+++ routers/OTP_Router.js
@@ -3,5 +3,5 @@ const { GetOTP, VerifyOTP, mobileOTP } = require("../Controller/OTP_Controller")
 const {isAuth}= require("../middleware/isAuth")
 router.post("/", GetOTP)
 router.post("/verify", VerifyOTP)
-// router.post("/mobile",isAuth, mobileOTP)
+router.post("/mobile",isAuth, mobileOTP)
 module.exports = router
