c07cb65a46dad6fdbfd6f3e31d79922c45890b70
ksj14-kumar <lionmassymassy@gmail.com>
2023-03-07 14:13:39 +0530

    stripe update

--- routers/stripe.js
+++ routers/stripe.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router()
 const { checkout_session, WebHook } = require('../Controller/Stripe_Payment_Controller');
 const { isAuth } = require('../middleware/isAuth');
-router.use(isAuth)
-router.post('/create-checkout-session', checkout_session);
+router.post('/create-checkout-session', isAuth,checkout_session);
 router.post('/webhook', WebHook);
 module.exports = router
